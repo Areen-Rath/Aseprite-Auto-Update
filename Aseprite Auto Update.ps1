c5ec4cdb99e65b17bcecba397d60abd6258228c3
Areen-Rath <areenrath0805@gmail.com>
2026-07-03 20:05:47 +0530

    Initial commit

---
+++ Aseprite Auto Update.ps1
@@ -0,0 +1,59 @@
+Add-Type -AssemblyName PresentationFramework
+
+<#
+    CMake, Ninja, Git and GitHub CLI must be installed and added
+    to the system PATH for this script to work properly.
+#>
+
+<#
+    Set the paths to Aseprite, Skia, and Visual Studio Developer Command Prompt.
+    In case of a different installation path or version, please change
+    the paths below accordingly.
+    For Visual Studio 2022, use the following path instead:
+    C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat
+#>
+$asepritePath = "C:\aseprite"
+$skiaPath = "C:\deps\skia"
+$VSPath = "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
+
+Set-Location $asepritePath
+& "$asepritePath\build\bin\aseprite.exe"
+
+try {
+    $current = git describe --tags
+    $latest = gh release view --json tagName --jq ".tagName"
+    if ($current -ne $latest) {
+        $choice = [System.Windows.MessageBox]::Show("A newer version of Aseprite is avilable. Update?", "Confirm Action", "YesNo", "Question")
+        if ($choice -eq "Yes") {
+            try {
+                Write-Output "Updating Aseprite..."
+                Write-Output "Pulling latest changes from GitHub..."
+                git pull
+                Write-Output "Updating Submodules..."
+                git submodule update --init --recursive
+                Write-Output "Checking out the latest stable release..."
+                git checkout $latest
+                Write-Output "Building Aseprite..."
+                & $VSPath -arch=x64
+                Remove-Item -Path "$asepritePath\build\*"
+                Set-Location $asepritePath\build
+                cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia -DSKIA_DIR="$skiaPath" -DSKIA_LIBRARY_DIR="$skiaPath\out\Release-x64" -DSKIA_LIBRARY="$skiaPath\out\Release-x64\skia.lib" -G Ninja ..
+                ninja aseprite
+                Write-Output "Aseprite updated successfully!"
+                Write-Output "You are now on version $latest."
+                Write-Output "Please close Aseprite and restart it to apply the update."
+                pause
+            } catch {
+                Write-Output "Aseprite build failed."
+                Write-Output "Please check the output above for any errors."
+                pause
+            }
+        } else {
+            exit
+        }
+    } else {
+        exit
+    }
+} catch {
+    exit
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Aseprite Auto Update
+
+Aseprite Auto Update is an app that automatically checks for the latest stable release for Aseprite and updates it if needed.
+
+**Note:** The app is only available on Windows and targets 64-bit devices.
+
+## Dependencies
+
+Dependencies for Aseprite:
+* Windows 11
+* Visual Studio Community 2026 (we don't support MinGW)
+* The Desktop development with C++ item + Windows SDK from Visual Studio installer
+* The latest version of CMake
+* Ninja build system
+* A compiled version of a supported branch of the Skia library
+
+Dependencies for the App:
+* Git for Windows
+* Github CLI
+
+The app assumes that:
+* CMake, Ninja, Git and GitHub CLI are added to PATH.
+* Visual Studio 2026 is being used.
+* The compiled version of Skia library is saved in `C:\deps\skia`.
+* Aseprite folder location is `C:\aseprite`.
+
+In case of any discrepancy, please refer to the comments in the PowerShell file for steps to address them.
+
+## How It Works
+
+Under the hood, the app is simply a PowerShell script that has been converted to an executable file using the `ps2exe` module and given the Aseprite logo. The PowerShell script opens Aseprite and checks for any updates by comparing the current version of the local Aseprite Git repo and the latest release of the GitHub repo. In case of a newer version, a dialog box will ask the user whether to update or not, and the user can decide accordingly. If Aseprite is up-to-date, the PowerShell script will exit and the user can continue doing their work.
+
+In case of a poor/no Internet connection, the script will exit automatically without notification. Success/failure of an update will be shown at the end in the PowerShell window.
+
+The PowerShell window will be visible for the entirety of the duration of the script run to ensure transparency and to enable the user to check the status of the update.
