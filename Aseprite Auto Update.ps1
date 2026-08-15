187be3e8718d54b12294e594f756d1f62cd8125d
Areen-Rath <areenrath0805@gmail.com>
2026-08-15 20:12:07 +0530

    Fixing CMake being unable to identify compiler

--- Aseprite Auto Update.ps1
+++ Aseprite Auto Update.ps1
@@ -14,44 +14,92 @@ Add-Type -AssemblyName PresentationFramework
 #>
 $asepritePath = "C:\aseprite"
 $skiaPath = "C:\deps\skia"
-$VSPath = "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
+$VSPath = "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\Launch-VsDevShell.ps1"
+
+$env:GIT_REDIRECT_STDERR = '2>&1'
 
 Set-Location $asepritePath
-& "$asepritePath\build\bin\aseprite.exe"
 
-try {
-    $current = git describe --tags
-    $latest = gh release view --json tagName --jq ".tagName"
-    if ($current -eq $latest) {
-        exit
-    }
-    $choice = [System.Windows.MessageBox]::Show("A newer version of Aseprite is avilable. Update?", "Confirm Action", "YesNo", "Question")
-    if ($choice -eq "No") {
-        exit
-    }
-    try {
-        Write-Output "Updating Aseprite..."
-        Write-Output "Pulling latest changes from GitHub..."
-        git pull
-        Write-Output "Updating Submodules..."
-        git submodule update --init --recursive
-        Write-Output "Checking out the latest stable release..."
-        git checkout $latest
-        Write-Output "Building Aseprite..."
-        & $VSPath -arch=x64
-        Remove-Item -Path "$asepritePath\build\*"
-        Set-Location $asepritePath\build
-        cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia -DSKIA_DIR="$skiaPath" -DSKIA_LIBRARY_DIR="$skiaPath\out\Release-x64" -DSKIA_LIBRARY="$skiaPath\out\Release-x64\skia.lib" -G Ninja ..
-        ninja aseprite
-        Write-Output "Aseprite updated successfully!"
-        Write-Output "You are now on version $latest."
-        Write-Output "Please close Aseprite and restart it to apply the update."
-        pause
-    } catch {
-        Write-Output "Aseprite build failed."
-        Write-Output "Please check the output above for any errors."
-        pause
-    }
-} catch {
+$current = git describe --tags
+if (-not $?) {
+    Write-Output "Something went wrong while checking the current version of Aseprite. Please check the output above for more information."
+    pause
     exit
 }
+
+$latest = gh release view --json tagName --jq ".tagName"
+if (-not $?) {
+    Write-Output "Something went wrong while checking for latest version on GitHub. Please check the output above for more information."
+    pause
+    exit
+}
+
+if ($current -eq $latest) {
+    exit
+}
+
+$choice = [System.Windows.MessageBox]::Show("A newer version of Aseprite is available. Update?`nNOTE: This will close any instance of Aseprite.", "Confirm Action", "YesNo", "Question")
+if ($choice -eq "No") {
+    & "$asepritePath\build\bin\aseprite.exe"
+    exit
+}
+Get-Process -Name "aseprite" -ErrorAction SilentlyContinue | Stop-Process -Force
+
+Write-Output "Updating Aseprite..."
+Write-Output "`nPulling latest changes from GitHub..."
+git reset --hard HEAD
+git checkout main
+git pull
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Something went wrong while pulling the latest changes from GitHub. Please check the output above for more information."
+    pause
+    exit
+}
+
+Write-Output
+Write-Output "`nUpdating submodules..."
+git submodule update --init --recursive
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Something went wrong while pulling the latest changes from GitHub. Please check the output above for more information."
+    pause
+    exit
+}
+
+Write-Output "`nOpening Visual Studio Developer Command Prompt..."
+& $VSPath -Arch amd64 -HostArch amd64
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Something went wrong while opening Visual Studio Developer Command Prompt. Please check the output above for more information."
+    pause
+    exit
+}
+
+Write-Output "`nCleaning up build directory..."
+Remove-Item -Path "$asepritePath\build\*" -Force -Recurse
+Set-Location $asepritePath\build
+
+$version = ($latest -replace "v") + "-dev"
+Write-Output "Changing version in CMakeLists.txt to $version..."
+$lines = Get-Content -Path "$asepritePath\src\ver\CMakeLists.txt"
+$lines[4] = "set(VERSION `"$version`")"
+$lines | Set-Content -Path "$asepritePath\src\ver\CMakeLists.txt"
+
+Write-Output "`nConfiguring the build with CMake..."
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia -DSKIA_DIR="$skiaPath" -DSKIA_LIBRARY_DIR="$skiaPath\out\Release-x64" -DSKIA_LIBRARY="$skiaPath\out\Release-x64\skia.lib" -G Ninja ..
+Write-Progress -Activity "CMake Configuration" -Completed
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Something went wrong while configuring the build with CMake. Please check the output above for more information."
+    pause
+    exit
+}
+
+Write-Output "`nBuilding Aseprite with Ninja..."
+ninja aseprite
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Something went wrong while building Aseprite. Please check the output above for more information."
+    pause
+    exit
+}
+
+(New-Object -ComObject Shell.Application).ShellExecute("$asepritePath\build\bin\aseprite.exe", "", "", "", 4)
+
+exit
